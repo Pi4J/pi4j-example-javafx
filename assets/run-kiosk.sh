9d15bb8463184244e5d3aad39aa1363be84fded1
Frank Delporte <frank@webtechie.be>
2021-04-21 22:06:39 +0200

    Extending run scripts to include kiosk mode

--- assets/run-kiosk.sh
+++ assets/run-kiosk.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+/sbin/init 3
+export ENABLE_GLUON_COMMERCIAL_EXTENSIONS=true
 java \
-  -Dglass.platform=gtk \
+  -Degl.displayid=/dev/dri/card0 \
+  -Dmonocle.egl.lib=/opt/arm32fb-sdk/lib/libgluon_drm-1.1.3.so \
   -Djava.library.path=/opt/arm32fb-sdk/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
+  -Dmonocle.platform=EGL \
   --module-path .:/opt/arm32fb-sdk/lib \
   --add-modules javafx.controls \
   --module com.pi4j.example/com.pi4j.example.JavaFxExample $@
+/sbin/init 5

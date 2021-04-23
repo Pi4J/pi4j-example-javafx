b08c7d5c4093587b72217c190b98f9d5a9c1e5d8
Frank Delporte <frank@webtechie.be>
2021-04-23 13:19:24 +0200

    Correct javafx-sdk17 path

--- assets/run-kiosk.sh
+++ assets/run-kiosk.sh
@@ -3,13 +3,13 @@
 export ENABLE_GLUON_COMMERCIAL_EXTENSIONS=true
 java \
   -Degl.displayid=/dev/dri/card0 \
-  -Dmonocle.egl.lib=/opt/javafx-sdk17/lib/libgluon_drm-1.1.3.so \
-  -Djava.library.path=/opt/javafx-sdk17/lib \
+  -Dmonocle.egl.lib=/opt/javafx-sdk-17/lib/libgluon_drm-1.1.3.so \
+  -Djava.library.path=/opt/javafx-sdk-17/lib \
   -Dmonocle.platform.traceConfig=false \
   -Dprism.verbose=false \
   -Djavafx.verbose=false \
   -Dmonocle.platform=EGL \
-  --module-path .:/opt/javafx-sdk17/lib \
+  --module-path .:/opt/javafx-sdk-17/lib \
   --add-modules javafx.controls \
   --module com.pi4j.example/com.pi4j.example.JavaFxExample $@
 /sbin/init 5
